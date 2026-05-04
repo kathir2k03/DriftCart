d9edd5b403bb737f201b0a2fb1d556fbde0646e0
kathir2k03 <m.kathiravan2003@gmail.com>
2026-05-04 07:19:12 +0530

    order apis till get all orders completed

--- server/routes/order.js
+++ server/routes/order.js
@@ -1,8 +1,10 @@
 const express = require('express')
-const { newOrder } = require('../controllers/orderController')
-const { isAuthenticatedUser } = require('../middleware/authenticate')
+const { newOrder, getSingleOrder, orders } = require('../controllers/orderController')
+const { isAuthenticatedUser, authorizeRoles } = require('../middleware/authenticate')
 const router = express.Router()
 
-router.route('/order/new').post(isAuthenticatedUser ,newOrder)
+router.route('/order/new').post(isAuthenticatedUser, newOrder)
+router.route('/order/:id').get(isAuthenticatedUser, getSingleOrder)
+router.route('/orders').get(isAuthenticatedUser, authorizeRoles('admin'), orders)
 
 module.exports = router
