8b63536a168f7dd54e06a08cd2583e457d865973
kathir2k03 <m.kathiravan2003@gmail.com>
2026-05-08 02:07:32 +0530

    Project Front End Setup, List Productlists and  deatils completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,36 @@
 import './App.css'
+import Home from './components/Home'
+import Footer from './components/layouts/Footer'
+import Header from './components/layouts/Header'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { HelmetProvider } from 'react-helmet-async'
+import { ToastContainer} from 'react-toastify'
+import ProductDetail from './components/product/ProductDetail'
 
 function App() {
 
+  const route = createBrowserRouter([
+  { path: '/', element:       <>
+        
+        <Home />
+       
+      </>},
+  { path: '/product-detail/:id', element: <ProductDetail/> },
+  // { path: '*', element: <Footer/> }
+])
+
   return (
-    <>
-    <h1></h1>
-    </>
+    <div className='App'>
+      <HelmetProvider>
+        <Header />
+        <div className="container container-fluid">
+          
+        <ToastContainer theme='dark'/>
+        <RouterProvider router={route} />
+         <Footer />
+        </div>
+      </HelmetProvider>
+    </div>
   )
 }
 
